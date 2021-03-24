fb786968d6e4ce06830a52b140a92423eba6016d
milandolez <73084411+milandolez@users.noreply.github.com>
2021-03-23 20:30:13 -0700

    Update pt_and_temp.R

--- pt_and_temp.R
+++ pt_and_temp.R
@@ -1,13 +1,5 @@
-#visualize and plan lol
-
-#head(tpoints@coords)
-#coords.x1 coords.x2
-#[1,]  357.6522  81.98904
-#[2,]  334.0000  62.00000
-#[3,]  893.0000 442.00000
-#[4,]  385.0000 442.00000
-#[5,]  361.6515  74.68428
-#[6,]  357.6831 101.10987
+#This code takes the key from "key_construction.R" and the coordinates from "xeno plot.R" and assigns a temperature to each xeno attachment.
+#I saved the output of "xeno plot.R" as "tpoints.RData" and the output of the key_construction.R" as "keytransparent.Rdata"
 
 #head(key)
 #scalevalues temperature
@@ -18,15 +10,49 @@
 #5         159    32.25455
 #6         140    31.41818
 
-setwd("~/Desktop")
-ir <- raster("IR_dorsal transparent.png")
+library(raster)
+ir <- raster("IR_dorsal transparent.JPG")
+
+load("tpoints.RData")  
+load("key_transparent.RData")
+
+tp <- coordinates(tpoints)
+
+#need to convert tpoints axes to image axes
+#Vivienne's code
+
+#tpoints x 0:912, y 608:0
+#image x 0:480, y 0:284
+
+rescale_x <- function(x, oldmax, newmax) {((x-0)/(oldmax - 0) * newmax)} 
+
+rescale_y <- function(x, oldmax, newmax) {newmax - ((x-0)/(oldmax - 0) * newmax) } 
+
+x2 <- rescale_x(tp[,1], 912, 480)
+y2 <- rescale_y(tp[,2], 608, 284)
+
+plot(ir)
+points(x2, y2)
+points(x2[260:262], y2[260:262], col="red", pch=16) #anchor points on thermal image
+
 
 #get values from the raster of xeno points
-scale<-extract(ir, tpoints@coords)
+scale<-extract(ir, x2, y2)
 
 temp_coord <- data.frame(tpoints@coords[,1],
                       tpoints@coords[,2],
                         scale)
+#> head(temp_coord)
+#tpoints.coords...1. tpoints.coords...2. scale
+#1            564.2781            80.38176   255
+#2            591.0000            78.00000   255
+#3            537.0000           454.00000   255
+#4             30.0000           444.00000   255
+#5            507.7976           110.18031   255
+#6            528.5589            92.63845   255
 
-#translate values to temperature
-temp <- scale 
+#hmm... they are all 255
+
+#translate color values to temperature using the key (still working on this)
+
+temp_coord$temperature <- ifelse(temp_coord$scale[i]==key$scalevalues, key$temperature, NA)
